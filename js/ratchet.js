81c5f7c9dd1b219c3e5f7019a7110f3ee61fe851
Ryan Stewart <ryan@ryanstewart.net>
2012-11-07 10:17:49 -0800

    Removed * from whitelist (didn't need) and improved ratchet.js to only support status 0 when on the filesystem.

--- js/ratchet.js
+++ js/ratchet.js
@@ -271,7 +271,7 @@
 
     xhr.onreadystatechange = function () {
 	  if (options._timeout) clearTimeout(options._timeout);
-      if (xhr.readyState == 4) xhr.status == 200 || xhr.status == 0 ? success(xhr, options) : failure(options.url);
+      if (xhr.readyState == 4) xhr.status == 200 || (xhr.status == 0 && options.url.indexOf('file:///') != -1) ? success(xhr, options) : failure(options.url);
     };
 
     if (!PUSH.id) {
